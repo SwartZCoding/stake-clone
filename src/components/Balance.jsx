bf996f2e69da47b3b5fe6cf2f22a36aa7a01671c
Anthony <mineof13if@gmail.com>
2021-01-17 21:40:48 +0100

    Start Working on Balance component

---
+++ src/components/Balance.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
+export default function Balance() {
+    return (
+
+        <div className="flex items-center w-1/3">
+            <div className="w-full background-balance mx-5 relative"></div>
+        </div>
+    );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import content from '../content';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
+import Balance from "./Balance";
 
 export default function Navigation() {
     return (
         <div className="navbar-color">
             <div className="flex items-center justify-between w-10/12 mx-auto py-5 text-white font-dosis">
                     <LazyLoadImage src={content.header.logo}/>
+                        <Balance/>
 
             <div onClick={event => window.open("https://discord.gg/7FhT8J85Z3")} className="w-4 mr-0">
                 <LazyLoadImage src={content.header.profile}/>
